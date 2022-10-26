6f1a5b26ae10ae57fd171795d8a4fd4f33d7b407
TonyaSmetanina <53794265+TonyaSmetanina@users.noreply.github.com>
2022-10-26 23:47:41 +0300

    Push the project

---
+++ script/index.js
@@ -0,0 +1,35 @@
+//  Створіть сторінку коментарів та стилізуйте її як сторінка відгуків. 
+//  За допомогою методів перебору виведіть на сторінку коментарі, пошту, номер коментаря та автора
+//  Дані тут : https://jsonplaceholder.typicode.com/comments
+
+console.log(data);
+
+function createElement(tagName, text, className) {
+    const element = document.createElement(tagName);
+    element.textContent = text;
+    element.classList.add(className); 
+    
+    return element
+}
+
+
+data.forEach(function(element) {
+    const {name, email, body} = element; 
+    const arr = [createElement ("div", name, "name"),createElement ("div", email, "email"), createElement ("div", body, "review")]
+
+    document.querySelector(".root").append(...arr); 
+
+    let userName = document.querySelector("#user")
+    userName.innerHTML = `Name: ${name}`;
+
+    let emailUser = document.querySelector("#email");
+    emailUser.innerHTML = `Email: ${email}`;
+
+    let reviewUser = document.querySelector("#review");
+    reviewUser.innerHTML = `Review: ${body}`;
+
+
+})
+
+
+document.querySelector(".root").prepend(userName, emailUser, reviewUser); 
